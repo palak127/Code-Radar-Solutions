a01a1d61576e0304d7fc7aabacfcda37299b2fb5
palak127 <189537036+palak127@users.noreply.github.com>
2025-02-07 18:25:31 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -21,11 +21,11 @@ int main() {
     }
     else if(a>=60 || a<70)
     {
-        printf("D")
+        printf("D");
     }
     else if(a<60)
     {
-        printf("F")
+        printf("F");
     }
 
     return 0;
